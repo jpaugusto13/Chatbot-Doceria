66967ec16aac8c958573f610e69eacdf5617b87e
jpaugusto13 <jpaugusto13@proton.me>
2023-12-13 07:11:08 -0300

    .

--- src/config/startChatBot.ts
+++ src/config/startChatBot.ts
@@ -12,7 +12,7 @@ function startChatbot(client : Whatsapp) {
     let seconds = date.getSeconds();
 
     if(hour == 10 && minutes == 0 && seconds == 0) {
-      client.sendText("120363180369706429@g.us", "Olá bom dia! Hoje temos 3 Kits").then((result) => console.log('Result: ', result))
+      client.sendText("120363180369706429@g.us", "Olá bom dia!").then((result) => console.log('Result: ', result))
       return "";
     }
   }, 800)
@@ -24,34 +24,21 @@ function startChatbot(client : Whatsapp) {
     text = text.replace("ç","c");
 
     if(text.includes("contato")) {
-      client.sendContactVcard(message.from, '558586169028@c.us', 'Doceria').then((result) => console.log('Result: ', result))
+      client.sendContactVcard(message.from, '558587234258@c.us', 'Doceria').then((result) => console.log('Result: ', result))
     }
 
     if(text.includes("endereco") || text.includes("localizacão")) {
       client.sendText(message.from, 'Rua 11 N° 131 \nJereissati 1 - Maracanaú \n(Referência: Drummond Noivas) ').then((result) => console.log('Result: ', result))
-      client.sendLocation(message.from, '-13.6561589', '-69.7309264', 'Brasil').then((result) => console.log('Result: ', result))
       return "";
     }
 
-    if (text.includes("bom dia") || text.includes("boa tarde") || text.includes("boa noite")) {
-      saudacoes(message, client);
-      return "";
-    }
-    
-    if(text.includes("encomenda")) {
-      await client.sendImage(message.from,"https://zildjian.com/cdn/shop/articles/aaron_spears_profile_1.png?v=1684240723", "" ,"")
-      .then((result) => console.log('Result: ', result))
-      return "";
-    }
-
+    // if (text.includes("bom dia") || text.includes("boa tarde") || text.includes("boa noite")) {
+    //   saudacoes(message, client);
+    //   return "";
+    // }
     
     if (text.includes("pix")) {
       pix(message, client);
-      return "";  
-    }
-
-    if(text.includes("produtos") || text.includes("servicos") || text.includes("tem bolo")) {
-      messageProdutos(message, client);
       return "";
     }
   });
--- src/messages/messageProdutos.ts
+++ src/messages/messageProdutos.ts
@@ -12,15 +12,14 @@ const messageProdutos = async (message: Message, client: Whatsapp) => {
   const produtos = await api.get("client/produtos").then(({ data }) => data.produtos);
   
   let produtosText : string = ""
+  let sizeArray = produtos.length;
   
-  produtos.forEach(({nome, preco, quantidade} : TypeProduto) => {
-    if(quantidade != 0) produtosText += `${nome} R$ ${Number(preco).toFixed(2).replace(".",",")}\n`
+  produtos.forEach((produto : TypeProduto, index: number) => {
+    console.log(index+++":"+sizeArray)
+    if(produto.quantidade != 0) produtosText += `${produto.nome} R$ ${Number(produto.preco).toFixed(2).replace(".",",")}${index++ != sizeArray ? "\n" : ""}`
   })
 
   await client.sendText(message.from, `Cardápio do dia \n\n${produtosText == "" ? "Estamos sem estoque no momento" : produtosText}`)
-  .then((result) => {
-    console.log('Result: ', result);
-  })
-};
+  .then((result) => console.log('Result: ', result))};
 
 export default messageProdutos;
